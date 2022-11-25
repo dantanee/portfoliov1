810684bb2c0d39c5f0f6b912eadecf70fb41e61c
dev.gee <dantaneecodes@gmail.com>
2022-11-25 17:39:00 +0100

    second

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), macrosPlugin()],
+// });
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+});
